af05b151bc5702ea88235cd09379ccf7de196fe3
Zhenya <maltaerman2@gmail.com>
2023-06-25 17:17:21 +0300

    fix build errors

--- src/components/Inputs/InputSearch.vue
+++ src/components/Inputs/InputSearch.vue
@@ -1,62 +1,50 @@
-<!-- eslint-disable vue/no-mutating-props -->
 <template>
   <label
-    class="input-1 flex flex-nowrap gap-2 border"
-    :class="{
-      'border-blue-c-500': isFocused,
-      'border-gray-c-300': !isFocused,
-    }"
+    :class="[
+      'input-1 flex flex-nowrap gap-2 border',
+      isFocused ? 'border-blue-c-500' : 'border-gray-c-300',
+    ]"
   >
     <div class="h-full w-5">
-      <SVG_search
-        class="mt-0.5 h-auto w-full"
-        :class="{
-          'fill-blue-c-500': isFocused,
-          'fill-gray-c-500': !isFocused,
-        }"
+      <SVGSearch
+        :class="[
+          'mt-0.5 h-auto w-full',
+          isFocused ? 'fill-blue-c-500' : 'fill-gray-c-500',
+        ]"
       />
     </div>
     <input
-      v-model="modelValue"
+      v-model="model"
       class="w-full outline-none"
-      :placeholder="placeholder"
       type="text"
-      @focusin="OnFocus(true)"
-      @focusout="OnFocus(false)"
+      v-bind="{ placeholder }"
+      @focusin="isFocused = true"
+      @focusout="isFocused = false"
     >
   </label>
 </template>
 
-<!-- eslint-disable camelcase -->
-<script>
-import SVG_search from '../ComponentsSVG/Icons/SVG_search.vue'
+<script setup>
+import { ref, watchEffect } from 'vue'
 
-export default {
-  name: 'InputSearch',
-  components: { SVG_search },
-  props: {
-    // eslint-disable-next-line vue/require-default-prop
-    modelValue: String,
-    // eslint-disable-next-line vue/require-default-prop
-    placeholder: String,
-  },
-  emits: ['update:modelValue'],
-  data() {
-    return {
-      isFocused: false,
-    }
-  },
-  watch: {
-    modelValue(val) {
-      this.$emit('update:modelValue', val)
-    },
-  },
-  methods: {
-    OnFocus(value) {
-      this.isFocused = value
-    },
-  },
-}
+import SVGSearch from '../ComponentsSVG/Icons/SVG_search.vue'
+
+const props = defineProps({
+  modelValue: { type: String, default: '' },
+  placeholder: { type: String, default: '' },
+})
+
+const isFocused = ref(false)
+
+const model = ref('')
+
+const emits = defineEmits(['update:modelValue'])
+
+watchEffect(() => {
+  emits('update:modelValue', model.value)
+})
+
+watchEffect(() => {
+  model.value = props.modelValue
+})
 </script>
-
-<style scoped></style>
--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,7 @@ import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 import { defineConfig } from 'vite'
 import svgLoader from 'vite-svg-loader'
 import vue from '@vitejs/plugin-vue'
-
 import eslint from 'vite-plugin-eslint'
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,19 +15,14 @@ export default defineConfig({
     vue(),
     eslint(),
     VueI18nPlugin({}),
-    // svgLoader({
-    //   defaultImport: 'url', // or 'raw'
-    // }),
-    // VueI18nPlugin({
-    //   include: resolve(
-    //     dirname(fileURLToPath(import.meta.url)),
-    //     './src/libs/i18n/locales/**',
-    //   ),
-    // }),
+    svgLoader({
+      defaultImport: 'url', // or 'raw'
+    }),
+    VueI18nPlugin({
+      include: resolve(
+        dirname(fileURLToPath(import.meta.url)),
+        './src/libs/i18n/locales/**',
+      ),
+    }),
   ],
-  // resolve: {
-  //   alias: {
-  //     '@types/cypress': path.resolve(__dirname, 'cypress.d.ts'),
-  //   }
-  // },
 });
